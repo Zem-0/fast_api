889995f27da322bfc16c2eaa04c7131fb2bb3591
parinith-19 <parinith@hospicepalliativereview.com>
2025-06-07 16:14:31 +0530

    push

--- app/routers/meetings.py
+++ app/routers/meetings.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models, schemas, crud
-from ..database import get_db
+from app.database import get_db
 
 router = APIRouter()
 
